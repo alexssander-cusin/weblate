938a3237f29e4444f203fff3661a4651c6ff5aad
Alexssander Cusin <alexssander.cusin@atlastechnol.com>
2025-05-01 13:04:00 -0300

    edit example

--- lang/en/texts.php
+++ lang/en/texts.php
@@ -3,4 +3,13 @@ return [
     'welcome' => 'Welcome to our application',
     'apples' => 'There is one apple|There are many apples',
     'new' => 'new',
+    'array' => [
+      'new' => 'new array',
+      'nested' => [
+        'new' => 'new nested array',
+        'another' => [
+          'new' => 'new another nested array',
+        ],
+      ],
+    ]
 ];
